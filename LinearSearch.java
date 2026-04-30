c43617c7fd200fb85075f638a7ddfaea41de6703
banana1312 <ban074839@gmail.com>
2026-04-30 13:21:17 +0900

    2-6-3

---
+++ LinearSearch.java
@@ -0,0 +1,34 @@
+/**
+ * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
+ *
+ * LinearSearch.java
+ *
+ */
+package chap2.s6.q3.advanced;
+
+import util.StandardInput;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+
+		// 1.数値が昇順に入力済みの配列を定義する。
+
+
+		// 2.変数を定義する。
+
+
+
+
+		// 3.データを検索する（探索ループ）。
+
+
+
+
+
+
+
+		// 4.結果を表示する。
+
+	}
+}
