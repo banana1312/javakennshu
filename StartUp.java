7efefd98f83c8102e0998575692f84f869dfa52e
banana1312 <ban074839@gmail.com>
2026-05-01 16:46:40 +0900

    5-2-4

--- StartUp.java
+++ StartUp.java
@@ -4,33 +4,42 @@
  * StartUp.java
  *
  */
-package chap5.s2.q3.standard;
+package chap5.s2.q4.advanced;
 
-
+import util.StandardInput;
 
 public class StartUp {
 
 	public static void main(String[] args) {
 
-		// a.tryブロックを開始する。
 
 
-			// 1.int型の配列を宣言し、初期化する。
+			// 1.String型の配列を宣言し、初期化する。
 
 
-			// 2.入力を促すメッセージを表示する。
+			// 2.見つかった文字列を格納する変数foundStrを宣言し、nullで初期化する。
 
 
-			// 3.表示したい要素の要素番号を入力させる。
+			// 3.入力を促すメッセージを表示する。
 
 
-			// 4.入力された要素番号の要素の値を表示する。
+			// 4.検索したい文字列を入力させる。
 
 
-		// b.catchブロックを記述する。
+			// 5.入力された文字列に一致する配列の要素を探索する。
 
 
-			// c.catchブロックの処理でメッセージを表示する。
+
+
+
+
+
+			// 6.変数foundStrを大文字に変換して表示する。
+
+
+		// 7.文字列が見つからなかった場合、大文字への変換の処理で発生する標準例外NullPointerExceptionをキャッチして
+		//   メッセージを表示する。
+
 
 
 	}
