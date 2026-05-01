36d4bfe8581cb94e9fc554ce714d18fb4277ea65
banana1312 <ban074839@gmail.com>
2026-05-01 16:42:44 +0900

    5-2-3

--- StartUp.java
+++ StartUp.java
@@ -4,42 +4,35 @@
  * StartUp.java
  *
  */
-package chap5.s1.q6.advanced;
+package chap5.s2.q2.standard;
 
-import util.StandardInput;
+
+
+
 
 public class StartUp {
 
 	public static void main(String[] args) {
 
-//		// 1.tryブロックを開始する。
-//
-//
-//			// ShoppingCartオブジェクトを生成する。
-//			ShoppingCart sc = new ShoppingCart();
-//
-//			// 商品を3つ追加する。
-//			sc.addItem(new Item("1", "Jaw-dropping chocolate", 500));
-//			sc.addItem(new Item("2", "Spicy Tuna Sandwitches", 1000));
-//			sc.addItem(new Item("3", "Tiger Shrimp Cocktail", 2500));
-//
-//			System.out.println("【商品検索】");
-//
-//			// 検索する商品のコードを入力する。
-//			System.out.print("  検索商品コード: ");
-//			String itemCode = StandardInput.readString();
-//			System.out.println();
-//
-//			// 商品を検索する。
-//			Item item = sc.findItem(itemCode);
-//
-//			// 商品の情報を表示する。
-//			item.showInfo();
-//
-//		// 2.ItemNotFoundExceptionをキャッチして処理するcatchブロックを記述する。
-//
-//
-//			// エラーメッセージを表示する。
+		// a.tryブロックを開始する。
+
+
+			// 1.標準入力された整数を格納する変数を宣言する。
+
+
+			// 2.入力を促すメッセージを表示する。
+
+
+			// 3.標準入力から整数を入力させる。
+
+
+			// 4.入力された整数を表示する。
+
+
+		// b.catchブロックを記述する。
+
+
+			// c.catchブロックの処理でエラーメッセージを表示する。
 
 
 	}
