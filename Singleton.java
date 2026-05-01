708a3c7a7ec21fa456746bd17b2d752ebfe6faec
banana1312 <ban074839@gmail.com>
2026-05-01 09:43:59 +0900

    3-2-7

---
+++ Singleton.java
@@ -0,0 +1,32 @@
+/**
+ * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
+ *
+ * Singleton.java
+ *
+ */
+package chap3.s2.q7.advanced;
+
+public class Singleton {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ StartUp.java
@@ -0,0 +1,29 @@
+/**
+ * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
+ *
+ * StartUp.java
+ *
+ */
+package chap3.s2.q7.advanced;
+
+public class StartUp {
+
+	public static void main(String[] args) {
+
+
+
+		// メンバ変数の値を確認する（初期値）。
+
+
+
+		// メンバ変数の値を確認する（変更後）。
+
+
+
+
+
+
+		// メンバ変数の値を確認する（オブジェクトの共有）。
+
+	}
+}
