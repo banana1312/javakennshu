92d9876b172bf31191448b8d29374b10dbdcfd18
banana1312 <ban074839@gmail.com>
2026-05-01 16:30:35 +0900

    5-1-3

---
+++ BmiCalculator.java
@@ -0,0 +1,43 @@
+/**
+ * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
+ *
+ * BmiCalculator.java
+ *
+ */
+package chap5.s1.q3.standard;
+
+public class BmiCalculator {
+
+	// メンバ変数
+	private double weight;
+	private double height;
+	private double bmi;
+
+	// 1.身長を設定するsetHeight()メソッド(セッター)を定義する。
+	//   引数が0以下の場合は、NegativeHeightExceptionを発生させる。
+
+
+
+
+
+
+
+	// 2.体重を設定するsetWeight()メソッド(セッター)を定義する。
+	//   引数が0以下の場合は、NegativeWeightExceptionを発生させる。
+
+
+
+
+
+
+
+	// 3.BMI値を取得するgetBmi()メソッド(ゲッター)を定義する。
+
+
+
+
+	// 4.BMI値を計算するcalculateBmi()メソッドを定義する。
+
+
+
+}
